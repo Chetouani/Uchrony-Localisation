a776dfae5584042187f0c23882c2d43f3b79d14c
Chetouani <chetouaniabdelhalim@gmail.com>
2015-04-16 14:51:31 +0200

    Version avec 4 iBeacons kontakt au coin de la piéce dans la salle metting2

--- app/src/main/java/be/uchrony/uchrony_localisation/MainActivity.java
+++ app/src/main/java/be/uchrony/uchrony_localisation/MainActivity.java
@@ -1,7 +1,42 @@
 package be.uchrony.uchrony_localisation;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.RemoteException;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.kontakt.sdk.android.connection.OnServiceBoundListener;
+import com.kontakt.sdk.android.device.BeaconDevice;
+import com.kontakt.sdk.android.device.Region;
+import com.kontakt.sdk.android.factory.Filters;
+import com.kontakt.sdk.android.manager.BeaconManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+import android.os.Handler;
 
 
 public class MainActivity extends ActionBarActivity {
