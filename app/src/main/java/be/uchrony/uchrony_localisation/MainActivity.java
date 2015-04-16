bd83b0a3fd4cfd35b898885e36f343da8bcb873a
Chetouani <chetouaniabdelhalim@gmail.com>
2015-04-16 14:48:33 +0200

    Version avec 4 iBeacons kontakt au coin de la piéce dans la salle metting2

--- app/src/main/java/be/uchrony/uchrony_localisation/MainActivity.java
+++ app/src/main/java/be/uchrony/uchrony_localisation/MainActivity.java
@@ -1,9 +1,7 @@
 package be.uchrony.uchrony_localisation;
 
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 
 public class MainActivity extends ActionBarActivity {
